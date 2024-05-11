421c588a7a0a599a8072c39f52396f843e16f542
ziyad-github <ziyadqsr@gmail.com>
2024-05-10 18:31:13 -0700

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(100):
     for num2 in range(100, -1, -1):
-        if sorted(str(num)) != sorted(str(num2)):
-            print("{}".format(num), end=", ")
+        if (sorted(str(num)) != sorted(str(num2))):
+            print("{:02d}".format(num), end=", ")
