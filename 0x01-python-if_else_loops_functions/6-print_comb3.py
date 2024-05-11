b34246ff406a4f85602a8e538a26b34e8f661d59
ziyad-github <ziyadqsr@gmail.com>
2024-05-10 18:30:18 -0700

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,4 @@
 for num in range(100):
     for num2 in range(100, -1, -1):
         if sorted(str(num)) != sorted(str(num2)):
-            if (num == 99):
-                print("89")
-                continue
             print("{}".format(num), end=", ")
