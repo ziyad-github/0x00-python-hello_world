23b29965d722833e4770c1b478400814025de067
ziyad-github <ziyadqsr@gmail.com>
2024-05-10 17:03:25 -0700

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter in 'abcdefghijklmnopqrstuvwxyz':
-    if(letter == "q" or letter == "e"):
+    if (letter == "q" or letter == "e"):
         continue
     print("{}".format(letter), end="")
