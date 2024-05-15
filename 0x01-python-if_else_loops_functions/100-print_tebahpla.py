3b53ef48c1ea43723b41b980d7f09f4a959a7c07
ziyad-github <ziyadqsr@gmail.com>
2024-05-14 19:09:12 -0700

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter in range(122, 96, -1):
     if (letter % 2 == 0):
-        letter = chr(ord(letter) - 32)
-    print("{:s}".format(letter))
+        letter = letter - 32
+    print("{:s}".format(chr(ord(letter)))
