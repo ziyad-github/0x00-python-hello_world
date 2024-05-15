3f81845bf8428bf1550a6fe74c135c8dbc4c308a
ziyad-github <ziyadqsr@gmail.com>
2024-05-14 18:12:26 -0700

    fix

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if (ord("a") <= ord(letter) < ord("z")):
+            print("{}".format(chr(ord(letter) + 32)), end="")
+        else:
+            print("{}".format(letter))
